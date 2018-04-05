176368775f8b31e8254f13bd73baaf7acb4daf4c
Waseem <waseemkoya@gmail.com>
2018-04-05 19:12:57 +1200

    Got button working in javascript

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(function() {
   // console.log will log a message or object to the browser developer console
   console.log("page loaded...");
 
-  $("selector-goes-here").click(/* function for when the button is clicked goes here */);
+  $("button").click(ohHi);
   /*
    * TODO: You will need to use a css selector to get jQuery to find the button element in the page
    * Then you will need to make a new javascript function to do stuff for when the button
@@ -16,6 +16,10 @@ $(function() {
    * The function should call one of the functions below, and pass the other in as the callback...
    */
 });
+
+function ohHi() {
+	 $("#results-area").text("Oh Hi!");
+}
 
 function displayQuestionAndAnswer(question, answer) {
   $("#results-area").text("True or false?");
